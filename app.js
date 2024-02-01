b407516b11f824155aafe6f5023dc2b193cd35b3
Cheartsak <cheartsak555@gmail.com>
2024-02-01 11:48:44 +0700

    update route for connecting database

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ import helmet from "helmet";
 import bcrypt from "bcrypt";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
+import dotenv from "dotenv";
+import databaseClient from "./services/database.mjs";
 import { mockUserActivity } from "./data/mockUserActivity.js";
 import { mockUserInfo } from "./data/mockUserInfo.js";
+import { ObjectId } from "mongodb";
+
+const HOSTNAME = process.env.SERVER_IP || "localhost";
+const PORT = process.env.SERVER_PORT || 8000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,9 +25,10 @@ const storage = multer.diskStorage({
   },
 });
 
+dotenv.config();
 const upload = multer({ storage });
 const app = express();
-const port = 8000;
+// const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,43 +36,78 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(helmet());
 
-app.get("/user/activity/:userId", (req, res) => {
+app.get("/user/activity/:userId", async (req, res) => {
   const { userId } = req.params;
-  const data = [...mockUserActivity];
-  const userData = data.filter((user) => user.userId === Number(userId));
+  // const data = [...mockUserActivity];
+  // const userData = data.filter((user) => user.userId === Number(userId));
+  const userData = await databaseClient
+    .db()
+    .collection("user-activity")
+    .find({ userId: new ObjectId(userId) })
+    .toArray();
+  res.json({ count: userData.length, data: userData });
+});
+
+app.get("/user/info/:userId", async (req, res) => {
+  const { userId } = req.params;
+  // const data = [...mockUserInfo];
+  // const userData = data.filter((user) => user.userId === Number(userId));
+  const userData = await databaseClient
+    .db()
+    .collection("user-info")
+    .find({ _id: new ObjectId(userId) })
+    .toArray();
   res.json({ data: userData });
 });
 
-app.get("/user/info/:userId", (req, res) => {
-  const { userId } = req.params;
-  const data = [...mockUserInfo];
-  const userData = data.filter((user) => user.userId === Number(userId));
-  res.json({ data: userData });
-});
-
-app.post("/user/changePassword/:userId", (req, res) => {
+app.post("/user/changePassword/:userId", async (req, res) => {
   const { userId } = req.params;
   const { newPassword } = req.body;
-  const data = [...mockUserInfo];
   const saltRounds = 12;
   const hashedPassword = bcrypt.hashSync(newPassword, saltRounds);
-  const userData = data.filter((user) => user.userId === Number(userId))[0];
-  userData.password = hashedPassword;
-  res.json({ data: [userData] });
+  // const data = [...mockUserInfo];
+  // const userData = data.filter((user) => user.userId === Number(userId))[0];
+  // userData.password = hashedPassword;
+  await databaseClient
+    .db()
+    .collection("user-info")
+    .updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { password: hashedPassword } }
+    );
+  res.send("OK");
 });
 
-app.patch("/user/editProfile/:userId", upload.single("image"), (req, res) => {
-  const { userId } = req.params;
-  const { name, email, phoneNumber } = req.body;
-  const { filename } = req.file;
-  const data = [...mockUserInfo];
-  const userData = data.filter((user) => user.userId === Number(userId))[0];
-  userData.fullName = name;
-  userData.email = email;
-  userData.phone = phoneNumber;
-  userData.imagePath = `/uploads/${filename}`;
-  res.json({ data: [userData] });
-});
+app.patch(
+  "/user/editProfile/:userId",
+  upload.single("image"),
+  async (req, res) => {
+    const { userId } = req.params;
+    const { name, email, phoneNumber } = req.body;
+    const { filename } = req.file;
+    // const data = [...mockUserInfo];
+    // const userData = data.filter((user) => user.userId === Number(userId))[0];
+    // userData.fullName = name;
+    // userData.email = email;
+    // userData.phone = phoneNumber;
+    // userData.imagePath = `/uploads/${filename}`;
+    await databaseClient
+      .db()
+      .collection("user-info")
+      .updateOne(
+        { _id: new ObjectId(userId) },
+        {
+          $set: {
+            fullName: name,
+            email: email,
+            phone: phoneNumber,
+            imagePath: `/uploads/${filename}`,
+          },
+        }
+      );
+    res.send("OK");
+  }
+);
 
 // mock upload
 // app.patch("/user/:userId/uploads", upload.single("image"), (req, res) => {
@@ -81,6 +123,31 @@ app.patch("/user/editProfile/:userId", upload.single("image"), (req, res) => {
 //   res.json({ data: [{ id: todoId, imagePath: `/uploads/${filename}`, name }] });
 // });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+// initilize web server
+const currentServer = app.listen(PORT, HOSTNAME, () => {
+  console.log(
+    `DATABASE IS CONNECTED: NAME => ${databaseClient.db().databaseName}`
+  );
+  console.log(`SERVER IS ONLINE => http://${HOSTNAME}:${PORT}`);
 });
+
+const cleanup = () => {
+  currentServer.close(() => {
+    console.log(
+      `DISCONNECT DATABASE: NAME => ${databaseClient.db().databaseName}`
+    );
+    try {
+      databaseClient.close();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
+
+// cleanup connection such as database
+process.on("SIGTERM", cleanup);
+process.on("SIGINT", cleanup);
