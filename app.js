cf01bf0f786abebbaccc00108dcb0c50e362e6bc
Cheartsak <cheartsak555@gmail.com>
2024-02-04 19:26:48 +0700

    add authorization and change image upload to cloudinary

--- app.js
+++ app.js
@@ -5,27 +5,51 @@ import bcrypt from "bcrypt";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
 import databaseClient from "./services/database.mjs";
 import { mockUserActivity } from "./data/mockUserActivity.js";
 import { mockUserInfo } from "./data/mockUserInfo.js";
 import { ObjectId } from "mongodb";
+import { auth } from "./middlewares/auth.js";
 
 const HOSTNAME = process.env.SERVER_IP || "localhost";
 const PORT = process.env.SERVER_PORT || 8000;
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
-  },
-  filename: function (req, file, cb) {
-    const name = uuidv4();
-    const extension = file.mimetype.split("/")[1];
-    const filename = `${name}.${extension}`;
-    cb(null, filename);
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+async function uploadToCloudinary(req, res, next) {
+  // console.log("req.file", req.file);
+  const fileBufferBase64 = Buffer.from(req.file.buffer).toString("base64");
+  const base64File = `data:${req.file.mimetype};base64,${fileBufferBase64}`;
+  // console.log("fileBufferBase64", fileBufferBase64);
+  // console.log("base64File", base64File);
+  req.cloudinary = await cloudinary.uploader.upload(base64File, {
+    resource_type: "auto",
+  });
+  // console.log(req.cloudinary);
+  next();
+}
+
+// upload to local
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     const name = uuidv4();
+//     const extension = file.mimetype.split("/")[1];
+//     const filename = `${name}.${extension}`;
+//     cb(null, filename);
+//   },
+// });
+
 dotenv.config();
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const app = express();
 // const port = 8000;
@@ -36,7 +60,7 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(helmet());
 
-app.get("/user/activity/:userId", async (req, res) => {
+app.get("/user/activity/:userId", auth, async (req, res) => {
   const { userId } = req.params;
   // const data = [...mockUserActivity];
   // const userData = data.filter((user) => user.userId === Number(userId));
@@ -48,7 +72,7 @@ app.get("/user/activity/:userId", async (req, res) => {
   res.json({ count: userData.length, data: userData });
 });
 
-app.get("/user/info/:userId", async (req, res) => {
+app.get("/user/info/:userId", auth, async (req, res) => {
   const { userId } = req.params;
   // const data = [...mockUserInfo];
   // const userData = data.filter((user) => user.userId === Number(userId));
@@ -56,11 +80,12 @@ app.get("/user/info/:userId", async (req, res) => {
     .db()
     .collection("user-info")
     .find({ _id: new ObjectId(userId) })
+    .project({ password: 0 })
     .toArray();
   res.json({ data: userData });
 });
 
-app.post("/user/changePassword/:userId", async (req, res) => {
+app.post("/user/changePassword/:userId", auth, async (req, res) => {
   const { userId } = req.params;
   const { newPassword } = req.body;
   const saltRounds = 12;
@@ -75,16 +100,18 @@ app.post("/user/changePassword/:userId", async (req, res) => {
       { _id: new ObjectId(userId) },
       { $set: { password: hashedPassword } }
     );
-  res.send("OK");
+  res.status(200).send("OK");
 });
 
 app.patch(
   "/user/editProfile/:userId",
+  auth,
   upload.single("image"),
+  uploadToCloudinary,
   async (req, res) => {
     const { userId } = req.params;
     const { name, email, phoneNumber } = req.body;
-    const { filename } = req.file;
+    // const { filename } = req.file;
     // const data = [...mockUserInfo];
     // const userData = data.filter((user) => user.userId === Number(userId))[0];
     // userData.fullName = name;
@@ -101,7 +128,7 @@ app.patch(
             fullName: name,
             email: email,
             phone: phoneNumber,
-            imagePath: `/uploads/${filename}`,
+            imagePath: req.cloudinary.secure_url,
           },
         }
       );
@@ -126,6 +153,41 @@ app.patch(
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
 // });
+
+// mock login
+app.post("/mock/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await databaseClient
+    .db()
+    .collection("user-info")
+    .findOne({ email: email });
+
+  // Fetch user from database
+
+  if (!user) {
+    return res
+      .status(400)
+      .send({ error: { message: "Invalid email or password" } });
+  }
+  // Check password
+  const validPassword = bcrypt.compareSync(password, user.password);
+  if (!validPassword) {
+    return res
+      .status(400)
+      .send({ error: { message: "Invalid email or password" } });
+  }
+
+  res.send({ token: createJwt(email), userId: user._id });
+});
+
+function createJwt(email) {
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+  const token = jwt.sign({ id: email }, jwtSecretKey, {
+    expiresIn: "1h",
+  });
+
+  return token;
+}
 
 // initilize web server
 const currentServer = app.listen(PORT, HOSTNAME, () => {
